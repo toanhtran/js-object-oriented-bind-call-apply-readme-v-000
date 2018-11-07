da0a09d0368846e5c509772ddef94ba95b9f29ba
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-07 23:56:54 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -2,3 +2,17 @@
 function greet(customerOne, customerTwo) {
     console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
 }
+
+let sally = { name: 'Sally' };
+
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greet.bind(sally); // newGreet is context-bound to sally
+
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+
+greet('Bob');
+// Hi Bob, my name is !
