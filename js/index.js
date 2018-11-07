a843e7b17e59f5d04258fad5994b16cf8323becc
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-07 23:56:28 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
 // use this to copy code snippets or us your browser's console
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
